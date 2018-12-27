972d8e7d5d7cac91df537a232892b1a2e7def4b3
Steven Kay <stevendkay@gmail.com>
2018-12-27 19:31:22 +0000

    promisify the render method which can take some time

--- client/src/app.js
+++ client/src/app.js
@@ -14,21 +14,30 @@ const randomizeGrid = function() {
 }
 
 const renderGrid = function() {
-  for (var row=0; row<100; row ++) {
-    for (var col=0; col<100; col++) {
-      const index = (row*100)+col;
-      const div = document.querySelector(`.cell_${index+1}`);
-      if (grid[index]===0) {
-        div.className = 'dead';
-      } else {
-        div.className = 'alive';
+
+  // render grid, this can take about a second so do as a promise
+
+  return new Promise((resolve,reject) => {
+    for (var row=0; row<100; row ++) {
+      for (var col=0; col<100; col++) {
+        const index = (row*100)+col;
+        const div = document.querySelector(`.cell_${index+1}`);
+        if (grid[index]===0) {
+          div.className = 'dead';
+        } else {
+          div.className = 'alive';
+        };
       };
     };
-  };
+    return resolve("Done!");
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM has loaded");
   randomizeGrid();
-  renderGrid();
+  renderGrid().then((done) => {
+    console.log("Grid refreshed!");
+  });
 });
