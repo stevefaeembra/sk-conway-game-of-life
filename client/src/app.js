8dd6a656b85e95607a1a73460dd16cb84198c1ec
Steven Kay <stevendkay@gmail.com>
2018-12-27 19:16:42 +0000

    fixed html to use 10000 cells, randomise start

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,20 @@
+let grid = new Array(1000);
+
+const randomizeGrid = function() {
+  for (var row=0; row<100; row ++) {
+    for (var col=0; col<100; col++) {
+      const index = (row*100)+col;
+      if (Math.random()>.95) {
+        grid[index] = 1;
+      } else {
+        grid[index] = 0;
+      }
+    }
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM has loaded");
+  randomizeGrid();
+  console.dir(grid);
 });
